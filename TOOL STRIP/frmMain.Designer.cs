18c47cf83534f19249cdab03cdadc652d852741d
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-07-14 10:16:06 +0100

    Add files via upload

---
+++ TOOL STRIP/frmMain.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace TOOL_STRIP
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator6 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnHome = new System.Windows.Forms.ToolStripButton();
+            this.btnPersons = new System.Windows.Forms.ToolStripButton();
+            this.btnTablet = new System.Windows.Forms.ToolStripButton();
+            this.btnNotes = new System.Windows.Forms.ToolStripButton();
+            this.btnCash = new System.Windows.Forms.ToolStripButton();
+            this.btnExit = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnHome,
+            this.toolStripSeparator1,
+            this.btnPersons,
+            this.toolStripSeparator2,
+            this.btnTablet,
+            this.toolStripSeparator3,
+            this.toolStripSeparator4,
+            this.btnNotes,
+            this.toolStripSeparator5,
+            this.btnCash,
+            this.toolStripSeparator6,
+            this.btnExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1029, 72);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 72);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 72);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 72);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 72);
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 72);
+            // 
+            // toolStripSeparator6
+            // 
+            this.toolStripSeparator6.Name = "toolStripSeparator6";
+            this.toolStripSeparator6.Size = new System.Drawing.Size(6, 72);
+            // 
+            // btnHome
+            // 
+            this.btnHome.Image = global::TOOL_STRIP.Properties.Resources._1House;
+            this.btnHome.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnHome.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(54, 69);
+            this.btnHome.Text = "Home ";
+            this.btnHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnPersons
+            // 
+            this.btnPersons.Image = global::TOOL_STRIP.Properties.Resources._2Persons;
+            this.btnPersons.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnPersons.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPersons.Name = "btnPersons";
+            this.btnPersons.Size = new System.Drawing.Size(54, 69);
+            this.btnPersons.Text = "Persons";
+            this.btnPersons.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnTablet
+            // 
+            this.btnTablet.Image = global::TOOL_STRIP.Properties.Resources._3Tablet;
+            this.btnTablet.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnTablet.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnTablet.Name = "btnTablet";
+            this.btnTablet.Size = new System.Drawing.Size(54, 69);
+            this.btnTablet.Text = "Tablet";
+            this.btnTablet.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnNotes
+            // 
+            this.btnNotes.Image = global::TOOL_STRIP.Properties.Resources._4Notes;
+            this.btnNotes.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnNotes.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNotes.Name = "btnNotes";
+            this.btnNotes.Size = new System.Drawing.Size(54, 69);
+            this.btnNotes.Text = "Notes";
+            this.btnNotes.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnCash
+            // 
+            this.btnCash.Image = global::TOOL_STRIP.Properties.Resources._5Cash;
+            this.btnCash.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnCash.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCash.Name = "btnCash";
+            this.btnCash.Size = new System.Drawing.Size(54, 69);
+            this.btnCash.Text = "Cash";
+            this.btnCash.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Image = global::TOOL_STRIP.Properties.Resources._6Exit;
+            this.btnExit.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(54, 69);
+            this.btnExit.Text = "Exit";
+            this.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1029, 377);
+            this.Controls.Add(this.toolStrip1);
+            this.IsMdiContainer = true;
+            this.Name = "frmMain";
+            this.Text = "Main Form";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnHome;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton btnPersons;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton btnTablet;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton btnNotes;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripButton btnCash;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator6;
+        private System.Windows.Forms.ToolStripButton btnExit;
+    }
+}
+
