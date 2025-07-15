2631fdb8d7f8e0e5ca27c10f4e013d5989b3b4b7
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-07-15 15:37:54 +0200

    Add files via upload

---
+++ PICTURE BOX/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PICTURE_BOX
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Strong;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Properties.Resources.Weak1;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(@"C:\Users\PC\Desktop\Facecook.png");
+        }
+    }
+}
