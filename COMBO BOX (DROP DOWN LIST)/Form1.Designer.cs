50340a11955b2f0636137a79898f1ebd6f3953ca
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-07-15 15:32:05 +0200

    Add files via upload

---
+++ COMBO BOX (DROP DOWN LIST)/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace COMBO_BOX__DROP_DOWN_LIST_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbPictures = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::COMBO_BOX__DROP_DOWN_LIST_.Properties.Resources.Pen;
+            this.pictureBox1.Location = new System.Drawing.Point(208, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(353, 283);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Red;
+            this.lblTitle.Location = new System.Drawing.Point(315, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(109, 40);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Title";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(173, 377);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Change Picture :";
+            // 
+            // cmbPictures
+            // 
+            this.cmbPictures.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbPictures.FormattingEnabled = true;
+            this.cmbPictures.Items.AddRange(new object[] {
+            "Book",
+            "Boy",
+            "Girl",
+            "Pen"});
+            this.cmbPictures.Location = new System.Drawing.Point(355, 367);
+            this.cmbPictures.Name = "cmbPictures";
+            this.cmbPictures.Size = new System.Drawing.Size(184, 32);
+            this.cmbPictures.Sorted = true;
+            this.cmbPictures.TabIndex = 3;
+            this.cmbPictures.SelectedIndexChanged += new System.EventHandler(this.cmbPictures_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbPictures);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "frmComboBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbPictures;
+    }
+}
+
---
+++ COMBO BOX (DROP DOWN LIST)/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMBO_BOX__DROP_DOWN_LIST_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbPictures.SelectedIndex = 0;
+        }
+
+        private void cmbPictures_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbPictures.SelectedItem.ToString().ToLower())
+            {
+                case "boy":
+                    pictureBox1.Image = Properties.Resources.Boy;
+                    lblTitle.Text = "Boy";
+                    break;
+
+                case "girl":
+                    pictureBox1.Image = Properties.Resources.Girl;
+                    lblTitle.Text = "Girl";
+                    break;
+                case "book":
+                    pictureBox1.Image = Properties.Resources.Book;
+                    lblTitle.Text = "Book";
+                    break;
+                case "pen":
+                    pictureBox1.Image = Properties.Resources.Pen;
+                    lblTitle.Text = "Pen";
+                    break;
+            }
+        }
+    }
+}
